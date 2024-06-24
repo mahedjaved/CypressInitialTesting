2d6ad264fff2f652b20f12f1107aa1d44550503b
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-24 08:31:54 +0100

    adds check for existence and visibility

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -4,7 +4,7 @@ describe("Assertions demo", () => {
     cy.visit("https://dribbble.com/search");
     // use implicit assertions:  a) should  b) and
     // check if URL contains "dribble.com"
-    cy.url().should("include", "dribble.com");
+    cy.url().should("include", "dribbble.com");
 
     // check if URL equals sth
     // cy.url().should("eq", "https://dribbble.com/search");
@@ -14,8 +14,22 @@ describe("Assertions demo", () => {
 
     // compacting shoulds with and
     cy.url()
-      .should("include", "dribble.com")
-      .and("contain", "dribble")
+      .should("include", "dribbble.com")
+      .and("contain", "dribbble")
       .and("eq", "https://dribbble.com/search");
+
+    // neagtive assertions
+    cy.url().should("not.contain", "dribblexx");
+
+    // capture the title
+    cy.title()
+        .should("not.include", "DribbleXX")
+        .and("not.eq", "OrangeH");
+
+    // capture the logo, check if it is visible
+    cy.get("a.nav-v2-logo")
+      .should("be.visible")
+      // or if it exists
+      .and("exist");
   });
 });
