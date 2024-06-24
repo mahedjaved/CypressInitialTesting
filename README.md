f79d0de4590d3413c8a58203a8f3f9194c732b31
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-24 17:24:49 +0100

    adds assertion examples for radio buttons and check buttons

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Interacting with Buttons, Radiobuttons And others
 
+* This branch mainly focuses on getting to validate, certain features/elements of a web page
+* First begining with interacting with `radio buttons` and `check boxes`
 * 
---
+++ cypress/e2e/accessradioNcheckboxes.cy.js
@@ -0,0 +1,18 @@
+describe("Examining UI Elements, including radiobuttons and ", () => {
+  it("Checking radio buttons", () => {
+    cy.visit("https://materializecss.com/radio-buttons.html");
+    // check whether radio button is visible or not
+    cy.get(":nth-child(1) > label > span").should("be.visible");
+    cy.get(":nth-child(2) > label > span").should("be.visible");
+    cy.get(":nth-child(3) > label > span").should("be.visible");
+    cy.get(":nth-child(4) > label > span").should("be.visible");
+
+    // selecting radio buttons, firstly select the first radio button
+    cy.get('label > input[name="group1"][type="radio"]').eq(2).check({ force: true });
+    // now check if it is selected, and others are deselected
+    cy.get('label > input[name="group1"][type="radio"]').eq(2).should("be.checked");
+    cy.get('label > input[name="group1"][type="radio"]').eq(0).should("not.be.checked");
+    cy.get('label > input[name="group1"][type="radio"]').eq(1).should("not.be.checked");
+    cy.get('label > input[name="group1"][type="radio"]').eq(3).should("not.be.checked");
+  });
+});
