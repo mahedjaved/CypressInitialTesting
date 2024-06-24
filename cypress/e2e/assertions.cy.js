9927007fd8b8acb68c8c7cde9ff3c28894c79fbd
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-24 07:40:57 +0100

    adds _and_ in replacement for _should_

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -1,3 +1,4 @@
+// const cypress = require("cypress");
 describe("Assertions demo", () => {
   it("Implicit assertions", () => {
     cy.visit("https://dribbble.com/search");
@@ -6,9 +7,15 @@ describe("Assertions demo", () => {
     cy.url().should("include", "dribble.com");
 
     // check if URL equals sth
-    cy.url().should("eq", "https://dribbble.com/search");
+    // cy.url().should("eq", "https://dribbble.com/search");
 
     // contains some part of url
-    cy.url().should("contain", "dribble");
+    // cy.url().should("contain", "dribble");
+
+    // compacting shoulds with and
+    cy.url()
+      .should("include", "dribble.com")
+      .and("contain", "dribble")
+      .and("eq", "https://dribbble.com/search");
   });
 });
