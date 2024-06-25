66e779bd251d2e6046055996dee56d86c5ea0f5e
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-25 07:44:34 +0100

    adds testing for checkbox checking exercise

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 * This branch mainly focuses on getting to validate, certain features/elements of a web page
 * First begining with interacting with `radio buttons` and `check boxes`
+* Sometimes, the options will be restricting the mouse movements, in this case use `force:true`
 * 
+
+<!-- below is good example website with radio buttons to play and access -->
+<!-- https://materializecss.com/radio-buttons.html -->
+<!-- the below website contains both radiobuttons and checkboxes -->
+<!-- https://www.ironspider.ca/forms/checkradio.htm -->
--- cypress/e2e/accesscheckboxes.cy.js
+++ cypress/e2e/accesscheckboxes.cy.js
@@ -1,29 +1,35 @@
 describe("Examining UI Elements, including radiobuttons and ", () => {
-    it("Checking radio buttons", () => {
-      cy.visit("https://materializecss.com/radio-buttons.html");
-      // check whether radio button is visible or not
-      cy.get(":nth-child(1) > label > span").should("be.visible");
-      cy.get(":nth-child(2) > label > span").should("be.visible");
-      cy.get(":nth-child(3) > label > span").should("be.visible");
-      cy.get(":nth-child(4) > label > span").should("be.visible");
-  
-      // selecting radio buttons, firstly select the first radio button
-      cy.get('label > input[name="group1"][type="radio"]')
-        .eq(2)
-        .check({ force: true });
-      // now check if it is selected, and others are deselected
-      cy.get('label > input[name="group1"][type="radio"]')
-        .eq(2)
-        .should("be.checked");
-      cy.get('label > input[name="group1"][type="radio"]')
-        .eq(0)
-        .should("not.be.checked");
-      cy.get('label > input[name="group1"][type="radio"]')
-        .eq(1)
-        .should("not.be.checked");
-      cy.get('label > input[name="group1"][type="radio"]')
-        .eq(3)
-        .should("not.be.checked");
-    });
+  // beforeEach runs preliminary commands before the test is executed
+  beforeEach(() => {
+    cy.visit("https://www.ironspider.ca/forms/checkradio.htm");
   });
-  
+  it("Checking checkbox buttons", () => {
+    // check visibility of checkbox
+    cy.get('input[name="color"]').eq(0).should("be.visible");
+
+    // select single checkbox
+    cy.get('input[name="color"]').eq(0).check();
+
+    // check if it is checked
+    cy.get('input[name="color"]').eq(0).should("be.checked");
+
+    // and the rest unchecked
+    cy.get('input[name="color"]').eq(1).should("not.be.checked");
+    cy.get('input[name="color"]').eq(2).should("not.be.checked");
+    cy.get('input[name="color"]').eq(3).should("not.be.checked");
+    cy.get('input[name="color"]').eq(4).should("not.be.checked");
+
+    // unselect checkbox
+    cy.get('input[name="color"]').eq(0).uncheck();
+    cy.get('input[name="color"]').eq(1).check();
+
+    // check if red is unchecked and yellow is checked, find an attribute that is shared across all the sub-elements
+    cy.get('input[name="color"]').eq(0).should("not.be.checked");
+    cy.get('input[name="color"]').eq(1).should("be.checked");
+
+    // check all checkboxes and uncheck
+    cy.get('input[type="checkbox"][name="color"]').check();
+    // cy.get('input[type="checkbox"][name="color"]').uncheck();
+    cy.get('input[type="checkbox"][name="color"]').should("be.checked");
+  });
+});
